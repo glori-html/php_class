56d02d100baf258e0297d6ade2a2393078dd5bc0
Glori <glorihuang@gmail.com>
2021-04-06 11:58:35 +0800

    php Day 4

--- 28.array-json.php
+++ 28.array-json.php
@@ -24,7 +24,11 @@ $people = [
 
 // header('Content-Type: application/json');
 
-echo json_encode($people, JSON_UNESCAPED_UNICODE);
+echo json_encode([
+    'get' => $_GET,
+    'persons' => $people
+], JSON_UNESCAPED_UNICODE);
+
 // 將json轉成字串用encode，字串轉回json用decode
 // json字串必須用雙引號，不可以有註解
 // 在JavaScript要將json轉成字串使用JSON.stringify(變數)
